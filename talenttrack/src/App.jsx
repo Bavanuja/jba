ca5b0744365ca2b68c216836d546ddfd2ff6b27f
Bavanuja <bbavanuja@gmail.com>
2025-02-03 20:28:46 +0530

    start talent track

---
+++ talenttrack/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+
+function App() {
+  return (
+    <div>
+      <h1>My React Website</h1>
+      <Button variant="contained" color="primary">Click Me</Button>
+    </div>
+  );
+}
+
+export default App;
